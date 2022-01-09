3f602cc8ddab39d54a7cb9c29f298e606d90877b
manoj kumar <manojkumar.sivalingam@gmail.com>
2022-01-09 13:41:32 +0530

    Section 6 End

--- DatingNetCoreAngular/Code/DatingApp/client/src/app/register/register.component.ts
+++ DatingNetCoreAngular/Code/DatingApp/client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,14 +10,17 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private accountService:AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
   register() {
     this.accountService.register(this.model).subscribe(response => {
       this.cancel();
-    }, error => { console.log(error)});
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    });
 
   }
   cancel() {

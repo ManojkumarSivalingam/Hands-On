aa4405c927185dcfc2bb92eb2bd8471f778f0f30
manoj kumar <manojkumar.sivalingam@gmail.com>
2023-04-07 14:52:42 +0530

    Section 7 End

--- DatingNetCoreAngular/Code/DatingApp/client/src/app/_interceptors/error.interceptor.ts
+++ DatingNetCoreAngular/Code/DatingApp/client/src/app/_interceptors/error.interceptor.ts
@@ -3,11 +3,13 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -15,6 +17,42 @@ export class ErrorInterceptor implements HttpInterceptor {
   constructor(private router:Router,private toastr:ToastrService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error:HttpErrorResponse) => {
+        if (error) {
+          switch (error.status) {
+            case 400:
+              if (error.error.errors) {
+                const modelStateErrors = [];
+                for (const key in error.error.errors) {
+                  if (error.error.errors[key]) {
+                    modelStateErrors.push(error.error.errors[key][0]);
+                  }
+                }
+                throw modelStateErrors;
+              }
+              else {
+                this.toastr.error(error.error, error.status.toString());
+              }
+              break;
+            case 401:
+              this.toastr.error("Unauthosized", error.status.toString());
+              break;
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500:
+              const navigationExtras: NavigationExtras = { state: { error: error.error } };
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+            default:
+              this.toastr.error('something went wrong');
+              console.log(error);
+              break;
+          }
+        }
+        throw error;
+      })
+    );
   }
 }
